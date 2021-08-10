a98c63b60c7e89420c95550e435437724643b5a4
abukta <abukta@gmail.com>
2021-08-10 13:45:59 -0700

    2 dimensional array

--- turorials/go/array.go
+++ turorials/go/array.go
@@ -8,8 +8,22 @@ func main() {
 	var a[5] int
 
 	a[4] = 100
-	fmt.Println("set:", a)
+	fmt.Println("===\nset:", a)
 	fmt.Println("get item [4]:", a[4])
     fmt.Println("length:", len(a))
 
+    // Declare and init array in one line - pretty cool
+	b := [5] int{1,2,3,4,5}
+	fmt.Println("====\ndcl:", b)
+
+	// Arrays types are one dimensional but can be used 
+	// to compose multi-dimensional data structures
+	var twoDArray [2][3] int
+	for i :=0; i<2; i++{
+		for j:=0;j<3;j++{
+			twoDArray [i][j]= i+j
+		}
+	}
+	fmt.Println("2d", twoDArray)
+	
 }
