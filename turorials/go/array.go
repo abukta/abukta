5916223724b6984ba04972309b7bdf0e14f269ad
abukta <abukta@gmail.com>
2021-08-10 11:12:50 -0700

    update

--- turorials/go/array.go
+++ turorials/go/array.go
@@ -2,9 +2,14 @@
 package main
 
 import "fmt"
-  
+
 // Main function
 func main() {
 	var a [5]int
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+    fmt.Println("length:", len(a))
 
 }
