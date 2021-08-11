6269b101278dc8aa05232f3d039954d0bbb16b04
abukta <abukta@gmail.com>
2021-08-10 19:25:02 -0700

    Updated in GoLand

--- turorials-go/hello/hello.go
+++ turorials-go/hello/hello.go
@@ -1,11 +1,12 @@
 // First Go program
 package main
-  
+
 import "fmt"
-  
+
 // Main function
 func main() {
 
-    fmt.Println("!... Hello World ...!")
-    fmt.Println(" test ")
+	fmt.Println("!... Hello World ...!")
+	fmt.Println(" test ")
+	fmt.Println("well this was dummy code :)")
 }
